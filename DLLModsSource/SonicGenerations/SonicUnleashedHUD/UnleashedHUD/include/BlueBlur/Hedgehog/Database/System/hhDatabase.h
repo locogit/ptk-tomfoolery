d865b4e4bdb0b27d56deda3b0809b096132a02c3
loco <61364136+locogit@users.noreply.github.com>
2023-03-19 15:24:36 -0400

    add

---
+++ DLLModsSource/SonicGenerations/SonicUnleashedHUD/UnleashedHUD/include/BlueBlur/Hedgehog/Database/System/hhDatabase.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include <Hedgehog/Base/hhObject.h>
+
+namespace Hedgehog::Base
+{
+    class CSharedString;
+}
+
+namespace Hedgehog::Database
+{
+    class CDatabase;
+    class CRawData;
+
+    static inline BB_FUNCTION_PTR(void, __thiscall, fpCDatabaseGetRawData, 0x40F010,
+        CDatabase* This, boost::shared_ptr<CRawData>& spRawData, const Hedgehog::Base::CSharedString& name, uint32_t unknown);
+
+    class CDatabase : public Base::CObject
+    {
+    public:
+        BB_INSERT_PADDING(0xEC);
+
+        virtual ~CDatabase() = default;
+
+        boost::shared_ptr<CRawData> GetRawData(const Base::CSharedString& name, uint32_t unknown = 0)
+        {
+            boost::shared_ptr<CRawData> spRawData;
+            fpCDatabaseGetRawData(this, spRawData, name, unknown);
+            return spRawData;
+        }
+    };
+
+    BB_ASSERT_SIZEOF(CDatabase, 0xF0);
+}
